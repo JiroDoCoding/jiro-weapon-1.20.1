09e77c15ab716819fcb56a263b7e98495636b655
Jiro <jaminxie7@gmail.com>
2025-11-21 23:29:40 -0500

    Initial commit

---
+++ src/main/java/net/boomjiro/jiroweapon/JiroWeapon.java
@@ -0,0 +1,16 @@
+package net.boomjiro.jiroweapon;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JiroWeapon implements ModInitializer {
+	public static final String MOD_ID = "jiro-weapon";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
